8e8f0b685030de2077e75f907e3dc60e7ed64eb9
Jesse Sullivan <11592718+Delune@users.noreply.github.com>
2018-06-22 12:57:34 -0400

    feat(secrets) added secrets.js to .gitignore

---
+++ secrets.js
@@ -0,0 +1,10 @@
+//google
+process.env.GOOGLE_CLIENT_ID =
+  '855188421213-1g3mniqv865q1b8d1r6gp6se93rrs9g7.apps.googleusercontent.com';
+process.env.GOOGLE_CLIENT_SECRET = 'QUJy6sAcVv6eaCL55pAo3rBM';
+process.env.GOOGLE_CALLBACK = '/auth/google/callback';
+
+//facebook
+process.env.FACEBOOK_APP_ID = '1980188775625291';
+process.env.FACEBOOK_APP_SECRET = '8319b49c070cfe050be8bca031c54e1d';
+process.env.FACEBOOK_CALLBACK = '/auth/facebook/callback';
