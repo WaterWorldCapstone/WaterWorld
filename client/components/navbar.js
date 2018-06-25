643579549641fa0af071f63d243296183bc90d06
Mouna Mallipeddi <mouna.mallipeddi@gmail.com>
2018-06-25 12:30:52 -0400

    feat(nav): options in Navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,33 +4,83 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 import AppBar from '@material-ui/core/AppBar'
+import {withStyles} from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <AppBar>
-    <div>
-      <nav>
-        {isLoggedIn ? (
-          <>'           '<span>
-              <h1>
-                WaterWorld <Link to="/">Home</Link>
-                <Link href="#" onClick={handleClick}>
-                  Logout
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flex: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+}
+const Navbar = props => {
+  const {classes, isLoggedIn} = props
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <i className="material-icons">=</i>{' '}
+            {/* originally MenuIcon; seems to not exist*/}
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            <Link to="/" className="navbar-link">
+              Waterworld
+            </Link>
+          </Typography>
+          <Button color="inherit">
+            <Link className="navbar-link" to="/">
+              Home
+            </Link>
+          </Button>
+          {isLoggedIn ? (
+            <div>
+              <Button color="inherit">
+                <Link className="navbar-link" to="/">
+                  LogOut
                 </Link>
-              </h1>
-                         </span>'         '</>
-        ) : (
-          <>'           '<span>
-              <h1>WaterWorld</h1>
-              <Link to="/">Home</Link>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-                         </span>'         '</>
-        )}
-      </nav>
-      <hr />
+              </Button>
+              <Button color="inherit">
+                <Link className="navbar-link" to="/signup">
+                  Edit
+                </Link>
+              </Button>
+            </div>
+          ) : (
+            <div>
+              <Button color="inherit">
+                <Link className="navbar-link" to="/login">
+                  Login
+                </Link>
+              </Button>
+              <Button color="inherit">
+                <Link className="navbar-link" to="/signup">
+                  SignUp
+                </Link>
+              </Button>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
     </div>
-  </AppBar>
-)
+  )
+}
+
+/*
+*/
 
 /**
  * CONTAINER
@@ -49,12 +99,13 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default withStyles(styles)(connect(mapState, mapDispatch)(Navbar))
 
 /**
  * PROP TYPES
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired
 }
