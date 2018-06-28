4c763c2e3d6ad317e58be064b6b72adc3cf6e7a2
Jesse Sullivan <11592718+Delune@users.noreply.github.com>
2018-06-28 16:19:49 -0400

    feat(Auction)added button and style with Material UI

--- client/components/auction.js
+++ client/components/auction.js
@@ -1,17 +1,52 @@
 import React, {Component} from 'react'
+import {Grid, withStyles, Typography, Button} from '@material-ui/core'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    textAlign: 'center'
+  }
+})
+// componentDidMount() {
+//   this.props.poolamount()
+// }
 
 class Auction extends Component {
-  // componentDidMount() {
-  //   this.props.poolamount()
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
   render() {
+    console.log(this.props)
+    const {classes} = this.props
     return (
       <div>
-        <h3>"Welcome Vendors"</h3>
+        <Grid
+          container
+          className={classes.root}
+          spacing={24}
+          id="auction-page-grid"
+          justify="center"
+        >
+          <Grid item xs={12}>
+            <Typography variant="title">"Welcome Vendors"</Typography>
+          </Grid>
+          <Link to="/auction/bid">
+            <Button variant="contained" color="primary">
+              Bid
+            </Button>
+          </Link>
+        </Grid>
       </div>
     )
   }
 }
+Auction.propTypes = {
+  classes: PropTypes.object.isRequired
+}
 
-export default Auction
+export default withStyles(styles)(Auction)
