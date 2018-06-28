9db2de650c0b9825583f8b960d3260609c4861fe
teresay <li.teresa.y@gmail.com>
2018-06-27 23:59:50 -0400

    feat(heatmap): sample data point added

---
+++ client/components/Map/HeatMap.js
@@ -0,0 +1,33 @@
+// import HeatmapLayer from "react-google-maps/lib/visualization/HeatmapLayer"
+
+// render(){
+
+// var data = [new window.google.maps.LatLng(37.782551, -122.445368),
+//             new window.google.maps.LatLng(37.782745, -122.444586),
+//             new window.google.maps.LatLng(37.782842, -122.443688),
+//             new window.google.maps.LatLng(37.782919, -122.442815),
+//             new window.google.maps.LatLng(37.782992, -122.442112),
+//             new window.google.maps.LatLng(37.783100, -122.441461)
+//       ]
+//   const Heatmap = withGoogleMap(props => (
+// <GoogleMap
+//     defaultZoom={1}
+//     center={{lat: 19.435031,lng: -99.167357}}
+// >
+// <HeatmapLayer data = {data} />
+// </GoogleMap>));
+
+// return(
+//   <div className="googleMap">
+// <Heatmap
+//   containerElement={
+//     <div style={{ height: `100%` }} />
+//   }
+//   mapElement={
+//     <div style={{ height: `100%` }} />
+//   }
+//   center={{ lat: -25.363882, lng: 131.044922 }}
+// />
+// </div>)
+// }
+// }
--- client/components/Map/Map.js
+++ client/components/Map/Map.js
@@ -1,36 +1,69 @@
 import React from 'react'
-const {compose, withProps, withStateHandlers} = require('recompose')
-const {
+import {compose, withProps} from 'recompose'
+import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker,
   InfoWindow
-} = require('react-google-maps')
+} from 'react-google-maps'
 import {gettingPools} from '../../store/pool'
 import {connect} from 'react-redux'
 import {MapInfoWindow} from './MapInfoWindow'
+import HeatmapLayer from 'react-google-maps/lib/components/visualization/HeatmapLayer'
+const {InfoBox} = require('react-google-maps/lib/components/addons/InfoBox')
 
-const HomeMap = compose(withScriptjs, withGoogleMap)(props => (
+export const HomeMap = compose(
+  withProps({
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyBEn6qWhn0flVzQ0uNzas6RCz9jYJT1xQM&v=3.exp&libraries=geometry,drawing,places,visualization',
+    loadingElement: <div style={{height: `100%`}} />,
+    containerElement: <div style={{height: `400px`}} />,
+    mapElement: <div style={{height: `100%`}} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
   <GoogleMap defaultZoom={3} defaultCenter={{lat: 40.705076, lng: -74.00916}}>
     {props.pools ? (
       props.pools.map(pool => <MapInfoWindow key={pool.id} pool={pool} />)
     ) : (
       <div />
     )}
+    <HeatmapLayer data={[new google.maps.LatLng(40.705076, -74.00916)]} />
+    {/* <InfoBox
+      defaultPosition={new google.maps.LatLng(40.705076, -74.00916)}
+      options={{ closeBoxURL: ``, enableEventPropagation: true }}
+    >
+      <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
+        <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
+          Hello, Taipei!
+        </div>
+      </div>
+    </InfoBox> */}
   </GoogleMap>
 ))
 
-const mapState = state => {
-  return {
-    pools: state.pool.allPools
-  }
-}
+// class HeatMap extends React.PureComponent {
 
-const mapDispatch = dispatch => {
-  return {
-    getPools: () => dispatch(gettingPools())
-  }
-}
+//   render() {
+//     return (
+//       <HomeMap pools={this.props.pools}/>
+//     )
+//   }
 
-export default connect(mapState, mapDispatch)(HomeMap)
+// }
+
+// const mapState = state => {
+//   return {
+//     pools: state.pool.allPools
+//   }
+// }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     getPools: () => dispatch(gettingPools())
+//   }
+// }
+
+// export default connect(mapState, mapDispatch)(HeatMap)
