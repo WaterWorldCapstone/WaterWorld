e2928a7ad1e0521705a2d69c86b488591c9d1bfa
dk268 <lorddanp@aol.com>
2018-07-03 02:36:25 -0400

    Feat(reducers): adds addDonation thunk creator to donation reducer

--- client/store/donation.js
+++ client/store/donation.js
@@ -13,6 +13,8 @@ const [LOADING_DONATION, LOADED_DONATION, ERROR_DONATION] = [
   'ERROR_DONATION'
 ]
 
+const ADD_DONATION = 'ADD_DONATION'
+
 export const getDonations = () => async dispatch => {
   try {
     dispatch({type: LOADING_DONATIONS})
@@ -33,6 +35,20 @@ export const getDonation = id => async dispatch => {
   }
 }
 
+export const addDonation = (donor, pool, amount) => async dispatch => {
+  try {
+    dispatch({type: LOADING_DONATIONS})
+    const newDonation = await Axios.post(`/api/donations/`, {
+      donorId: donor.id,
+      poolId: pool.id,
+      amount
+    })
+    dispatch({type: ADD_DONATION}, {payload: newDonation.data})
+  } catch (e) {
+    dispatch({type: ERROR_DONATIONS, payload: e})
+  }
+}
+
 const initialState = {allDonations: [], singleDonation: {}, status: LOADING}
 
 export default function(state = initialState, action) {
@@ -45,6 +61,12 @@ export default function(state = initialState, action) {
       return {...state, status: LOADING}
     case LOADED_DONATION:
       return {...state, status: LOADED, singleDonation: action.payload}
+    case ADD_DONATION:
+      return {
+        ...state,
+        status: LOADED,
+        allDonations: [...state.allDonations, action.payload]
+      }
     case ERROR_DONATION:
       return {...state, status: ERROR, error: action.payload}
     case ERROR_DONATIONS:
