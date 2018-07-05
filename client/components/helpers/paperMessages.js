6159ac33a6a137131298a774ac6aa8066a7054a8
dk268 <32686586+dk268@users.noreply.github.com>
2018-07-05 14:34:26 -0400

    Update paperMessages.js

--- client/components/helpers/paperMessages.js
+++ client/components/helpers/paperMessages.js
@@ -2,21 +2,18 @@ import React from 'react'
 import {Typography} from '@material-ui/core'
 
 export const paperMessages = [
-  <Typography style={{color: 'white'}} key="1">
-    Nearly 5,000 children die every day from a water-related illness…that's one
-    nearly every 20 seconds
+  <Typography style={{color: 'white'}} className="tick-me" key="1">
+    One child dies every twenty seconds from a water-related illness.
   </Typography>,
-  <Typography style={{color: 'white'}} key="2">
-    In many places where water scarcity exists, it is because the people living
-    there do not have the means or the money to access the water that is
-    directly beneath them
+  <Typography style={{color: 'white'}} className="tick-me" key="2">
+    Many die of thirst when there is water underfoot; they can't afford to reach
+    it.
   </Typography>,
-  <Typography style={{color: 'white'}} key="3">
-    Floods and other water-related disasters make up 70% of all deaths caused by
-    natural disasters
+  <Typography style={{color: 'white'}} className="tick-me" key="3">
+    Floods, and other water-related disasters, cause 70% of all deaths caused by
+    any natural disaster.
   </Typography>,
-  <Typography style={{color: 'white'}} key="4">
-    At least 1.8 billion people globally use a fecally contaminated drinking
-    source
+  <Typography style={{color: 'white'}} className="tick-me" key="4">
+    At least 1.8 billion people drink fecally contaminated water.
   </Typography>
 ]
